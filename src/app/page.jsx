8140d3836b44ff79dffea0e2ffb6d5bdce77a6d1
Gomdil <dark1288@naver.com>
2023-08-24 11:47:21 +0900

    2commit

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,41 +2,8 @@ import Image from "next/image";
 import Hero from "public/hero.png";
 import styls from "./page.module.css"
 import Button from "../components/Button/Button";
-import pool from '../utils/db';
-
-
-// function queryPromise(queryString) {  
-// 	return new Promise((resolve, reject) => {  
-// 		connection.query(queryString, (error, results) => {  
-// 			if (error) {  
-// 				return reject(error);  
-// 			}  
-// 			resolve(results);  
-// 		});  
-// 	});  
-// }
-
 
 export default async function Home() {
-  
-
-  //DB Query 
-  const getdate = async (queryString)=>{ 
-    try {      
-      const [rows] = await pool.execute(queryString);          
-      return rows;
-    } catch (error){
-      return [error];
-    }
-  }  
-
-
-  const queryString = "SELECT * FROM board"
-  const rows = await getdate(queryString);  
-  //const queryString = "SELECT * FROM board limit 1";
-  //const rows = await queryPromise(queryString);  
-  console.log(rows)    	
-
   return (
     <div className={styls.container}>
       <div className={styls.item}>
